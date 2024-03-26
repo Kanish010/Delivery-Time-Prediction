c06474f214a9942a814d97986108511b1be59f7c
Kanish010 <kanish.alluri@gmail.com>
2024-03-26 15:17:55 -0700

    init commit

---
+++ Planning.py
@@ -0,0 +1,38 @@
+# Supply Chain Optimization Classes
+
+# Demand Forecasting
+"""
+Chosen Model: Gradient Boosting Machine (GBM)
+Alternatives: Time Series Analysis (ARIMA, SARIMA)
+
+"""
+
+# Lead Time Prediction
+"""
+Chosen Model: Gradient Boosting Machine (GBM) + Neural Networks
+Alternatives: Time Series Analysis (ARIMA, SARIMA)
+
+"""
+
+# Inventory Optimization
+"""
+Chosen Model: Economic Order Quantity (EOQ) + Neural Networks 
+Alternatives: Stochastic Inventory Models
+
+"""
+
+# Supply Chain Risk Management 
+"""
+Chosen Model: Bayesian State-Space Models
+Alternatives: Dynamic Bayesians Networks, Stochastic Bayesian Networks
+
+"""
+
+# SQL Database
+"""
+Generate data from ChatGPT
+Use 1 large database to store all the data
+Keep 4 seperate codes to generate data anyway
+Add CSV to SQL database functionality
+
+"""
